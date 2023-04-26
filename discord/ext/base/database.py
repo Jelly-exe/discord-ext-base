68a199335f791bf5d1cf68ad586a5e7548884956
Jelly <Jellybob2016@outlook.com>
2023-04-27 00:31:45 +0100

    Make syntax easier

--- discord/ext/base/database.py
+++ discord/ext/base/database.py
@@ -1,19 +1,20 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 import sqlalchemy
 
 
 class Database:
-    def __init__(self, driver):
+    def __init__(self, driver: str, tables: Tuple[str]):
         self.engine = sqlalchemy.create_engine(driver)
         self.metadata = sqlalchemy.MetaData()
 
-        self.tables: Dict[str, sqlalchemy.Table] = self._get_tables()
+        self.tableKeys = tables
+        self._tables: Dict[str, sqlalchemy.Table] = self._get_tables()
 
     def get_table(self, name: str) -> sqlalchemy.Table:
-        if name.lower() not in self.tables:
+        if name.lower() not in self._tables:
             raise KeyError(f"Table {name} does not exist")
-        return self.tables[name.lower()]
+        return self._tables[name.lower()]
 
     def get_engine(self) -> sqlalchemy.Engine:
         return self.engine
@@ -27,7 +28,7 @@ class Database:
     def _get_tables(self) -> Dict[str, sqlalchemy.Table]:
         tables = {}
 
-        for table in self.metadata.tables.keys():
+        for table in self.tableKeys:
             tables[table.lower()] = Table(table, self.metadata, autoload_with=self.engine)
 
         return tables
